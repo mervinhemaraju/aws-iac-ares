25ef04c3acfb09126fb37d4ed3f1b98937a40430
Mervin Hemaraju <th3pl4gu33@gmail.com>
2024-01-25 17:23:11 +0400

    - Added new budget

---
+++ providers.tf
@@ -0,0 +1,26 @@
+# * Terraform Provider for AWS for the connection
+provider "aws" {
+
+  # * The AWS Environment Configurations
+  region = var.region
+
+  # * The Default Tags
+  default_tags {
+    tags = local.tags.default
+  }
+}
+
+# * The Terraform Module
+terraform {
+
+  # * Terraform version
+  required_version = ">= 1.0.0"
+
+  # * AWS Provider
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.9.0"
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,6 @@
+# > Secret variable
+# * Terraform AWS providers vars
+variable "region" {
+  type    = string
+  default = "eu-west-1"
+}
