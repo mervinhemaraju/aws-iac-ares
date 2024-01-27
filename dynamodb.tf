e82dce5a0196dfd1afacfbb072ed0b74bba0cb83
Mervin Hemaraju <th3pl4gu33@gmail.com>
2024-01-27 22:37:43 +0400

    - Removed dynamodb

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,44 +1,44 @@
-# * Create a Dynamo DB Table for the Project sandbox
-resource "aws_dynamodb_table" "sandbox" {
-  name         = "sandbox-testing"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "item_id"
+# # * Create a Dynamo DB Table for the Project sandbox
+# resource "aws_dynamodb_table" "sandbox" {
+#   name         = "sandbox-testing"
+#   billing_mode = "PAY_PER_REQUEST"
+#   hash_key     = "item_id"
 
-  attribute {
-    name = "item_id"
-    type = "S"
-  }
+#   attribute {
+#     name = "item_id"
+#     type = "S"
+#   }
 
-  attribute {
-    name = "item_name"
-    type = "S"
-  }
+#   attribute {
+#     name = "item_name"
+#     type = "S"
+#   }
 
-  attribute {
-    name = "item_category"
-    type = "S"
-  }
+#   attribute {
+#     name = "item_category"
+#     type = "S"
+#   }
 
-  attribute {
-    name = "item_status"
-    type = "S"
-  }
+#   attribute {
+#     name = "item_status"
+#     type = "S"
+#   }
 
-  global_secondary_index {
-    name            = "item_name_index"
-    hash_key        = "item_name"
-    projection_type = "ALL"
-  }
+#   global_secondary_index {
+#     name            = "item_name_index"
+#     hash_key        = "item_name"
+#     projection_type = "ALL"
+#   }
 
-  global_secondary_index {
-    name            = "item_status_index"
-    hash_key        = "item_status"
-    projection_type = "ALL"
-  }
+#   global_secondary_index {
+#     name            = "item_status_index"
+#     hash_key        = "item_status"
+#     projection_type = "ALL"
+#   }
 
-  global_secondary_index {
-    name            = "item_category_index"
-    hash_key        = "item_category"
-    projection_type = "ALL"
-  }
-}
+#   global_secondary_index {
+#     name            = "item_category_index"
+#     hash_key        = "item_category"
+#     projection_type = "ALL"
+#   }
+# }
