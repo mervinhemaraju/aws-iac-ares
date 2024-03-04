51e0c040bc5bde90ab1e3183680f634b7b5b02cd
Mervin Hemaraju <115166315+mervinhemaraju@users.noreply.github.com>
2024-03-04 20:46:32 +0400

    - Added a new dynamodb table for the project mo-bizin-travay (#5)
    * - Added a new dynamodb table for the project mo-bizin-travay
    
    * - Removed unused attributes in mo-bizin-travay db
    
    * - Fixed issue with dynamodb

--- dynamodb.tf
+++ dynamodb.tf
@@ -42,47 +42,48 @@ resource "aws_dynamodb_table" "jeeyah_bucket_list" {
   }
 }
 
-# # * Create a Dynamo DB Table for the Project sandbox
-# resource "aws_dynamodb_table" "sandbox" {
-#   name         = "sandbox-testing"
-#   billing_mode = "PAY_PER_REQUEST"
-#   hash_key     = "item_id"
+resource "aws_dynamodb_table" "mo_bizin_travay" {
+  name         = "mo-bizin-travay"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
 
-#   attribute {
-#     name = "item_id"
-#     type = "S"
-#   }
+  attribute {
+    name = "id"
+    type = "S"
+  }
 
-#   attribute {
-#     name = "item_name"
-#     type = "S"
-#   }
+  attribute {
+    name = "posted_date"
+    type = "S"
+  }
 
-#   attribute {
-#     name = "item_category"
-#     type = "S"
-#   }
+  attribute {
+    name = "recruiter"
+    type = "S"
+  }
 
-#   attribute {
-#     name = "item_status"
-#     type = "S"
-#   }
+  attribute {
+    name = "updated_at"
+    type = "S"
+  }
 
-#   global_secondary_index {
-#     name            = "item_name_index"
-#     hash_key        = "item_name"
-#     projection_type = "ALL"
-#   }
+  global_secondary_index {
+    name            = "recruiter_index"
+    hash_key        = "recruiter"
+    projection_type = "ALL"
+  }
 
-#   global_secondary_index {
-#     name            = "item_status_index"
-#     hash_key        = "item_status"
-#     projection_type = "ALL"
-#   }
+  global_secondary_index {
+    name            = "updated_at_index"
+    hash_key        = "updated_at"
+    projection_type = "ALL"
+  }
 
-#   global_secondary_index {
-#     name            = "item_category_index"
-#     hash_key        = "item_category"
-#     projection_type = "ALL"
-#   }
-# }
+  global_secondary_index {
+    name            = "posted_date_index"
+    hash_key        = "posted_date"
+    projection_type = "ALL"
+  }
+}
+
+
