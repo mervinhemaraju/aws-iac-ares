74759ac2be76571714b71fbabac45eacefcff3be
Mervin Hemaraju <th3pl4gu33@gmail.com>
2024-03-13 14:15:51 +0400

    - Added iam role and policies for github actions oidc

--- iam.tf
+++ iam.tf
@@ -1,5 +1,6 @@
 
-resource "aws_iam_openid_connect_provider" "default" {
+# Identity Providers
+resource "aws_iam_openid_connect_provider" "github" {
   url = "https://token.actions.githubusercontent.com"
 
   client_id_list = [
@@ -9,4 +10,31 @@ resource "aws_iam_openid_connect_provider" "default" {
   thumbprint_list = [
     data.tls_certificate.example.certificates[0].sha1_fingerprint
   ]
+}
+
+
+# IAM Roles
+resource "aws_iam_role" "oidc_github" {
+  name        = "github-actions"
+  description = "The role for GitHub to assume"
+  assume_role_policy = templatefile(
+    "${path.module}/policies/iam_oidc_github_role_trust.json",
+    {
+      oidc_federated_arn   = aws_iam_openid_connect_provider.github.arn
+      github_oidc_audience = "sts.amazonaws.com"
+    }
+  )
+  managed_policy_arns = [aws_iam_policy.lambda.arn]
+}
+
+
+# IAM Policies
+resource "aws_iam_policy" "oidc_github" {
+
+  name        = "github-actions"
+  path        = "/"
+  description = "The iam actions allowed for Github to execute"
+  policy = file(
+    "${path.module}/policies/iam_oidc_github_role_actions.json"
+  )
 }
