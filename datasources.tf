db86f0292e6a2280c088d93c672add73cf75478a
Mervin Hemaraju <th3pl4gu33@gmail.com>
2024-03-13 13:59:42 +0400

    - Deleted dynamodb mo-bizin-travay (moved to its own project stack) - Added identity provider for github actions

--- datasources.tf
+++ datasources.tf
@@ -1,2 +1,6 @@
 # * Define our data source to fetch secrets
 data "doppler_secrets" "prod_main" {}
+
+data "tls_certificate" "example" {
+  url = "https://token.actions.githubusercontent.com"
+}
---
+++ iam.tf
@@ -0,0 +1,12 @@
+
+resource "aws_iam_openid_connect_provider" "default" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com",
+  ]
+
+  thumbprint_list = [
+    data.tls_certificate.example.certificates[0].sha1_fingerprint
+  ]
+}
