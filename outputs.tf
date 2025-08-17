b112b5dcc933a5f8f34cbda211241e9db3680767
Mervin Hemaraju <th3pl4gu33@gmail.com>
2025-08-17 16:20:57 +0400

    - Removed unused outputs

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-# Output the contact me lambda function_url
-output "contact_me_lambda_function_url" {
-  value = module.contact_me.lambda_function_url
-}
+# # Output the contact me lambda function_url
+# output "contact_me_lambda_function_url" {
+#   value = module.contact_me.lambda_function_url
+# }
