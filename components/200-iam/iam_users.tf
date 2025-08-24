b3a447d1663648a710500f5c3c12e412a1baca55
Mervin Hemaraju <th3pl4gu33@gmail.com>
2025-08-25 00:34:05 +0400

    - Updated permission set relay - Removed unused sandbox user

--- components/200-iam/iam_users.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_iam_user" "sandbox" {
-  name = "sandbox"
-  path = "/"
-}
-
-resource "aws_iam_user_policy_attachment" "sandbox_administrator" {
-  user       = aws_iam_user.sandbox.name
-  policy_arn = data.aws_iam_policy.administrator.arn
-}
--- components/200-iam/sso_permission_set.tf
+++ components/200-iam/sso_permission_set.tf
@@ -3,12 +3,12 @@ resource "aws_ssoadmin_permission_set" "administrator_access" {
   instance_arn     = tolist(data.aws_ssoadmin_instances.this.arns)[0]
   name             = "AdministratorAccess"
   description      = "Full administrator access"
-  relay_state      = "https://ec2.console.aws.amazon.com/ec2/home?region=${var.region}#"
+  relay_state      = "https://${var.region}.console.aws.amazon.com/console/home?region=${var.region}#"
   session_duration = "PT6H"
 }
 
 resource "aws_ssoadmin_managed_policy_attachment" "administrators" {
   instance_arn       = tolist(data.aws_ssoadmin_instances.this.arns)[0]
-  managed_policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+  managed_policy_arn = data.aws_iam_policy.administrator.arn
   permission_set_arn = aws_ssoadmin_permission_set.administrator_access.arn
 }
