557bbe3cd7b211b6da8e7ad2f11f02407559529f
Mervin Hemaraju <th3pl4gu33@gmail.com>
2025-08-24 21:08:05 +0400

    -

--- components/200-iam/sso_users.tf
+++ components/200-iam/sso_users.tf
@@ -1,6 +1,6 @@
 # Administrator user and its assignment
 resource "aws_identitystore_user" "administrator" {
-  identity_store_id = tolist(data.aws_ssoadmin_instances.examthisple.identity_store_ids)[0]
+  identity_store_id = tolist(data.aws_ssoadmin_instances.this.identity_store_ids)[0]
 
   display_name = "Mervin Hemaraju"
   user_name    = "th3pl4gu3"
