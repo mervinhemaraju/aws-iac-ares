1554d5140a2d88f6293da31e5bd67bbf5d64933f
Mervin Hemaraju <th3pl4gu33@gmail.com>
2025-08-24 21:21:56 +0400

    - Added sso group and permission sets for administrators

--- components/200-iam/sso_groups.tf
+++ components/200-iam/sso_groups.tf
@@ -1,5 +1,6 @@
+# Administrator access sso group
 resource "aws_identitystore_group" "administrators" {
   identity_store_id = tolist(data.aws_ssoadmin_instances.this.identity_store_ids)[0]
-  display_name      = "adminsitrators"
+  display_name      = "administrators"
   description       = "Full Administrator access."
 }
---
+++ components/200-iam/sso_permission_set.tf
@@ -0,0 +1,14 @@
+# Administrator permission set and its attachment
+resource "aws_ssoadmin_permission_set" "administrator_access" {
+  instance_arn     = tolist(data.aws_ssoadmin_instances.this.arns)[0]
+  name             = "AdministratorAccess"
+  description      = "Full administrator access"
+  relay_state      = "https://ec2.console.aws.amazon.com/ec2/home?region=${var.region}#"
+  session_duration = "PT6H"
+}
+
+resource "aws_ssoadmin_managed_policy_attachment" "administrators" {
+  instance_arn       = tolist(data.aws_ssoadmin_instances.example.arns)[0]
+  managed_policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+  permission_set_arn = aws_ssoadmin_permission_set.administrator_access.arn
+}
