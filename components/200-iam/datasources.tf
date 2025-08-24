c4327c5d5939e961fdd867c5df7052891f461917
Mervin Hemaraju <th3pl4gu33@gmail.com>
2025-08-24 23:54:14 +0400

    - Added account assignment

--- components/200-iam/datasources.tf
+++ components/200-iam/datasources.tf
@@ -1,6 +1,9 @@
 # Iam Identity Center Instances
 data "aws_ssoadmin_instances" "this" {}
 
+# Get the current account
+data "aws_caller_identity" "current" {}
+
 # Get the TLS certificate from Github tokens
 data "tls_certificate" "github" {
   url = local.constants.oidc.github.url
--- components/200-iam/sso_groups.tf
+++ components/200-iam/sso_groups.tf
@@ -1,6 +1,17 @@
-# Administrator access sso group
+# Administrator access sso group and aws account access
 resource "aws_identitystore_group" "administrators" {
   identity_store_id = tolist(data.aws_ssoadmin_instances.this.identity_store_ids)[0]
   display_name      = "administrators"
   description       = "Full Administrator access."
+}
+
+resource "aws_ssoadmin_account_assignment" "current" {
+  instance_arn       = tolist(data.aws_ssoadmin_instances.this.arns)[0]
+  permission_set_arn = aws_ssoadmin_permission_set.administrator_access.arn
+
+  principal_id   = aws_identitystore_group.administrators.group_id
+  principal_type = "GROUP"
+
+  target_id   = data.aws_caller_identity.current.account_id
+  target_type = "AWS_ACCOUNT"
 }
